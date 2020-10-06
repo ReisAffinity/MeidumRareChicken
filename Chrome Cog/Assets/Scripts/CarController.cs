222e2e0f8f1ccabd770027901bf6ad3c6320a848
ReisAffinity <xagexskyx@gmail.com>
2020-10-06 12:09:05 -0700

    First Commit / Prototype usable

---
+++ Chrome Cog/Assets/Scripts/CarController.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody theRB;
+
+    public float maxSpeed;
+
+    public float forwardAccel = 8f, reverseAccel = 4f;
+
+    private float speedInput;
+
+    public float turnStrength = 180f; 
+    private float turnInput;
+
+    private bool grounded;
+
+    public Transform groundRayPoint, groundRayPoint2;
+    public LayerMask whatIsGround;
+    public float groundRayLength = .75f;
+
+    private float dragOnGround;
+    public float gravityMod = 5f;
+
+    public Transform leftFrontWheel, rightFrontWheel;
+    public float maxWheelTurn = 25f;
+
+    public ParticleSystem[] dustTrail;
+    public float maxEmission = 25f, emissionFadeSpeed = 20f;
+    private float emissionRate;
+
+    //Sound Effects
+    public AudioSource engineSound;
+    public AudioSource skidSound;
+    public float skidFadeSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB.transform.parent = null;
+
+        dragOnGround = theRB.drag;
+
+        emissionRate = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Going forward and backward
+        speedInput = 0f;
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardAccel;
+        }
+        else if(Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * reverseAccel;
+        }
+
+        turnInput = Input.GetAxis("Horizontal");
+
+        //Movement Air Control / Turn Left And Right
+
+        /*if(grounded && Input.GetAxis("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Mathf.Sign(speedInput) * (theRB.velocity.magnitude / maxSpeed), 0f));
+        }*/
+
+        //Turning the wheels
+        leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn) - 180, leftFrontWheel.localRotation.eulerAngles.z);
+        rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn), rightFrontWheel.localRotation.eulerAngles.z);
+
+        //transform.position = theRB.position;
+
+        //Particle Effect Control
+        emissionRate = Mathf.MoveTowards(emissionRate, 0f, emissionFadeSpeed * Time.deltaTime);
+
+        if (grounded && (Mathf.Abs(turnInput) > .5f || (theRB.velocity.magnitude < maxSpeed * .5f && theRB.velocity.magnitude != 0)))
+        {
+            emissionRate = maxEmission;
+        }
+
+        if (theRB.velocity.magnitude <= .5f)
+        {
+            emissionRate = 0;
+        }
+
+        for (int i = 0; i < dustTrail.Length; i++)
+        {
+            var emissionModule = dustTrail[i].emission;
+
+            emissionModule.rateOverTime = emissionRate;
+        }
+
+        // Engine Sound
+        if(engineSound != null)
+        {
+            engineSound.pitch = 1f + ((theRB.velocity.magnitude / maxSpeed) * 2f);
+        }
+
+        //Skid Sound
+        if(skidSound != null)
+        {
+            if(Mathf.Abs(turnInput) > 0.5f)
+            {
+                skidSound.volume = 1f;
+
+            }
+            else
+            {
+                skidSound.volume = Mathf.MoveTowards(skidSound.volume, 0f, skidFadeSpeed * Time.deltaTime);
+            }
+        }
+    }
+    // This happens every 0.2 seconds. A fixed force
+    private void FixedUpdate()
+    {
+        grounded = false;
+
+        RaycastHit hit;
+        Vector3 normalTarget = Vector3.zero;
+        
+        // These physics raycast will help adjust the car to the ramp.
+        if(Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatIsGround))
+        {
+            grounded = true;
+
+            normalTarget = hit.normal;
+        }
+
+        if(Physics.Raycast(groundRayPoint2.position, -transform.up, out hit, groundRayLength, whatIsGround))
+        {
+            grounded = true;
+
+            normalTarget = (normalTarget + hit.normal) / 2f;
+        }
+
+        //When on ground rotate to match the normal
+        if (grounded)
+        {
+            transform.rotation = Quaternion.FromToRotation(transform.up, normalTarget) * transform.rotation;
+        }
+
+        //Accelerates the car
+        if (grounded) {
+
+            theRB.drag = dragOnGround;
+
+            theRB.AddForce(transform.forward * speedInput * 1000f);
+        }
+        else
+        {
+            theRB.drag = .1f;
+
+            theRB.AddForce(-Vector3.up * gravityMod * 100f); 
+        }
+       
+        
+        if(theRB.velocity.magnitude > maxSpeed)
+        {
+            theRB.velocity = theRB.velocity.normalized * maxSpeed;
+        }
+
+        transform.position = theRB.position;
+
+        //Turn left or right of car when flying
+
+        if (grounded && Input.GetAxis("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Mathf.Sign(speedInput) * (theRB.velocity.magnitude / maxSpeed), 0f));
+        }
+    }
+}
