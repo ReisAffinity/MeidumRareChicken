2368fe95e99d3b8fe5d18348d44daddf91e332a5
Urbanham <Urbanham15@gmail.com>
2020-11-02 18:26:54 -0800

    addign item block

---
+++ Chrome Cog/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Item : ScriptableObject
+{
+    public string Name;
+    public string description;
+    public int Uses;
+
+    public Sprite Visual;
+
+}
