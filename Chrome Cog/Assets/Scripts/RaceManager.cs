aa94ad5dccc2460fa0dabad003828b80a1eadd5b
ReisAffinity <xagexskyx@gmail.com>
2020-11-11 18:57:31 -0800

    added a RubberBand System

--- Chrome Cog/Assets/Scripts/RaceManager.cs
+++ Chrome Cog/Assets/Scripts/RaceManager.cs
@@ -19,6 +19,12 @@ public class RaceManager : MonoBehaviour
     public float timeBetweenPosCheck = .2f;
     private float posCheckCounter;
 
+    //Rubber Band System
+    public float aiDefaultSpeed = 30f;
+    public float playerDefaultSpeed = 30f;
+    public float rubberBandSpeedMod = 3.5f;
+    public float rubberBandAccel = .5f;
+
     private void Awake()
     {
         instance = this;
@@ -70,6 +76,23 @@ public class RaceManager : MonoBehaviour
 
             UIManager.instance.positionText.text = playerPosition + "/" + (allAICars.Count + 1);
         }
+        
+        //Rubber Band Management
+        if(playerPosition == 1)
+        {
+            //Situation 1: Player ahead of ai Cars then ai Cars gets a boost
+            foreach(CarController aiCar in allAICars)
+            {
+                aiCar.maxSpeed = Mathf.MoveTowards(aiCar.maxSpeed, aiDefaultSpeed + rubberBandSpeedMod, rubberBandAccel * Time.deltaTime);
+            }
 
+            //speed gradually moves to player default speed - rubberspeedmod, (how fast we are changing it)
+            playerCar.maxSpeed = Mathf.MoveTowards(playerCar.maxSpeed, playerDefaultSpeed - rubberBandSpeedMod, rubberBandAccel * Time.deltaTime);
+        }
+        else
+        {
+            //Situation 2: if Player is behind Ai cars then they will get a certain boost. Basically they will get a boost based on their position in the game.
+            playerCar.maxSpeed = Mathf.MoveTowards(playerCar.maxSpeed, playerDefaultSpeed + (rubberBandSpeedMod * ((float)playerPosition / (allAICars.Count + 1))), rubberBandAccel * Time.deltaTime);
+        }
     }
 }
