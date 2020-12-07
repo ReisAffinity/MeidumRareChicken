fa3d35c6db7267d202cb22c829eb8b1f1b8d69e9
ReisAffinity <xagexskyx@gmail.com>
2020-12-07 04:17:55 -0800

    Added more menu Ui, Racer Track Selection, Etc

---
+++ Chrome Cog/Assets/Scripts/TrackSelectButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrackSelectButton : MonoBehaviour
+{
+    public string trackSceneName;
+
+    public Image trackImage;
+
+    public int raceLap = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectTrack()
+    {
+        RaceInfoManager.instance.trackToLoad = trackSceneName;
+        RaceInfoManager.instance.noOfLaps = raceLap;
+        RaceInfoManager.instance.trackSprite = trackImage.sprite;
+
+        MainMenu.instance.trackSelectImage.sprite = trackImage.sprite;
+
+        MainMenu.instance.CloseTrackSelect();
+    }
+}
